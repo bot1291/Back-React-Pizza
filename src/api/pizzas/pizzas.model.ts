d557db4b6b28f8925ea8d70479d7b10afd9be042
Igor Raskatov <aura.star@yandex.ru>
2022-12-18 02:38:39 +0300

    Add handler and messages for errors

--- src/api/pizzas/pizzas.model.ts
+++ src/api/pizzas/pizzas.model.ts
@@ -2,6 +2,8 @@ import { WithId } from 'mongodb';
 import * as z from 'zod';
 import { database } from '../../database';
 
+const collection = process.env.NODE_ENV ? 'pizzas' : 'test';
+
 export const SizeAndPrice = z.object({
 	size: z.string(),
 	price: z.number(),
@@ -9,14 +11,23 @@ export const SizeAndPrice = z.object({
 
 export const Pizza = z.object({
 	currentPrice: z.number(),
-	image: z.string(),
+	image: z.string().url(),
 	title: z.string(),
-	rating: z.number().max(1),
-	types: z.string().array(),
-	possibleDoughs: z.string().array(),
-	sizesAndPrices: SizeAndPrice.array(),
+	rating: z.number().max(1, { message: 'Rating must have 1 number' }),
+	types: z
+		.string()
+		.array()
+		.min(1, { message: 'Types must have at least 1 type' }),
+	possibleDoughs: z
+		.string()
+		.array()
+		.min(1, { message: 'Possible doughs must have at least 1 dough' }),
+	sizesAndPrices: SizeAndPrice.array().min(1, {
+		message:
+			'Size and price must have at least 1 dependency between each other',
+	}),
 });
 
 export type Pizza = z.infer<typeof Pizza>;
 export type PizzasWithId = WithId<Pizza>;
-export const Pizzas = database.collection<Pizza>('pizzas');
+export const Pizzas = database.collection<Pizza>(collection);
