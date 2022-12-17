afd9c12bc043cc32d80bb50140c2958dbf58d537
Igor Raskatov <aura.star@yandex.ru>
2022-12-17 20:00:39 +0300

    Create base structure

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import express, { json, Response, Request } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(json());
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.get('/', (_req: Request, res: Response) => {
+	res.json({
+		message: 'Started page',
+	});
+});
+
+app.use('/api');
+
+export default app;
