06a55f5d03115afc2366c8f3ae46e88913000678
Igor Raskatov <aura.star@yandex.ru>
2022-12-18 02:39:41 +0300

    Add ParamsWithId interface

--- src/api/pizzas/pizzas.routes.ts
+++ src/api/pizzas/pizzas.routes.ts
@@ -1,14 +1,28 @@
 import { Router } from 'express';
 import { validateRequest } from '../../middlewares';
+import { Pizza } from './pizzas.model';
 
 const router = Router();
 
 router.get('/');
-router.get('/:id', validateRequest({
-	params: 
-}));
+router.get(
+	'/:id',
+	validateRequest({
+		params: Pizza,
+	})
+);
 router.post('/');
-router.put('/:id');
-router.delete('/:id');
+router.put(
+	'/:id',
+	validateRequest({
+		params: Pizza,
+	})
+);
+router.delete(
+	'/:id',
+	validateRequest({
+		params: Pizza,
+	})
+);
 
 export default router;
